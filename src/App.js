130c13738ca098c2695032e705145d279cf68b1e
louaylou <ltifilouaykhalil@gmail.com>
2023-02-07 02:10:14 +0100

    mycheckpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from "react";
+import Form from "./Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container-fluid">
+      <header className="bg-primary text-white p-5">
+        <h1 className="text-center">My React App</h1>
       </header>
+      <main className="py-5">
+        <div className="container">
+          <Form />
+        </div>
+      </main>
     </div>
   );
 }
